efc85c890d9287e4f93fa7d6ee77a10f63b4d735
Cristian <cristianpaez009@gmail.com>
2024-01-30 07:24:19 -0500

    finish

--- api/src/middleware/auth.js
+++ api/src/middleware/auth.js
@@ -3,13 +3,13 @@ import 'dotenv/config';
 
 const authMiddleware = (req, _, next) => {
   const token = req.headers.authorization || '';
-
+  const key = process.env.SECRET_KEY || "UNSAFE_STRING"
   try {
     if (!token) {
       req.user = null;
       next();
     } else {
-      const decodedUser = jwt.verify(token, process.env.SECRET_KEY || "UNSAFE_STRING");
+      const decodedUser = jwt.verify(token, key);
       req.user = decodedUser;
       next();
     }
